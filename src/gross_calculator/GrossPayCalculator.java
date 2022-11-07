2d251609c627102ac26588b090ccfc932237dcb8
mckenney.m <matthew.t.mckenney@gmail.com>
2022-11-07 13:29:04 -0700

    refactored the calculator

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,9 +1,27 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("Hello");
+        // 1. Get the number of hours worked
+        int hours = 0;
+        System.out.println("How many hours did you work?");
+
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+
+        // 2. Get the hourly pay rate
+        double payRate = 0;
+        System.out.println("What is your pay rate?");
+        payRate = scanner.nextDouble();
+
+        scanner.close();
+
+        // 3. Multiply hours and pay rate
+        double grossPay = payRate * hours;
+        // 4. Display results
+        System.out.println("Gross pay: " + grossPay);
     }
 }
