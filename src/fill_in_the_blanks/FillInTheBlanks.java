dc18fc54ea950c2410e4fce96a5bcad6b3f164a0
mckenney.m <matthew.t.mckenney@gmail.com>
2022-11-09 10:59:38 -0700

    answering the question

---
+++ src/fill_in_the_blanks/FillInTheBlanks.java
@@ -0,0 +1,25 @@
+package fill_in_the_blanks;
+
+import java.util.Scanner;
+
+public class FillInTheBlanks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // ask user for a Season of the year
+        System.out.println("Please enter a season of the year: ");
+        String season = scanner.next();
+
+        // ask user for a int
+        System.out.println("Please enter a whole number: ");
+        int wholeNumber = scanner.nextInt();
+
+        // ask user for an adjective
+        System.out.println("Please enter an adjective: ");
+        String adjective = scanner.next();
+
+        // print answer
+
+
+    }
+}
